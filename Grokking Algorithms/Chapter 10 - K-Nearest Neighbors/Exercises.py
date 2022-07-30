9550573c71b6bed1e9d954ff2dd0525c754cc80d
George Beshay <georgebeshay101@gmail.com>
2022-07-30 22:58:00 +0200

    Add files via upload

---
+++ Grokking Algorithms/Chapter 10 - K-Nearest Neighbors/Exercises.py
@@ -0,0 +1,14 @@
+# E (10.1):
+#       By Using AI, The more available ratings for a user
+#          the more the system can determine something about the user's personality like being a choosier person.
+#       So After a function can be created that receives the user's rating as an input and then
+#       analyze it according to pre-collected data about the user's personality and output a rating which
+#       stands for the user rating and in the same time doesn't mislead the recommendation system algorithm.
+# ------------------------------------------------- SEPARATOR -------------------------------------------------
+# E(10.2):
+#       Same Answer as the previous question, the influencer rating can be pushed to a certain method that
+#       execute some calculations on the rating and output a final rating that doesn't mislead the system algorithm.
+# ------------------------------------------------- SEPARATOR -------------------------------------------------
+# E(10.3):
+#       Too Low.
+# ------------------------------------------------- SEPARATOR -------------------------------------------------
